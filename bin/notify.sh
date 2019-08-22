7482b3be38dcea8883780dee71e511983ca136ee
shun <shun.fix9@gmail.com>
2019-08-22 20:25:44 +0000

    fix: notify

--- bin/notify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-result=$1
-
-echo "$result : $channel / $timestamp"
-curl "$GETTO_PSYCHER_URL?$GETTO_PSYCHER_TOKEN=true&source=gitlab&result=$result&channel=$channel&timestamp=$timestamp"
